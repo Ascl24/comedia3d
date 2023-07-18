d44597da47da926d3dd1dfe44a0a948ba3835b8a
asclepias <nolanchansophy@proton.me>
2023-07-18 17:03:12 -0500

    Got a functional 3D camera for rotation on the x-axis, need to implement matrix math(?) for 360 degree rotations

--- main.c
+++ main.c
@@ -1,20 +1,23 @@
 #include "raylib.h"
 
+void updateCameraTarget(Vector3 *cameratarget); 
 int main(void) 
 {
 	const int screenWidth = 800;
 	const int screenHeight = 600;
+	const Vector3 origin = {0.0f, 0.0f, 0.0f};
+	bool canMove;
 
 	InitWindow(screenWidth, screenHeight, "COMEDIA");
 
 	Camera3D camera = { 0 };
-	camera.position = (Vector3){0.0f, 10.0f, 4.0f};
-	camera.target = (Vector3){0.0f, 0.0f, 0.0f };
+	camera.position = (Vector3){0.0f, 2.0f, 4.0f};
+	camera.target = (Vector3){0.0f, 2.0f, 0.0f };
 	camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
-	camera.fovy = 45.0f;
+	camera.fovy = 60.0f;
 	camera.projection = CAMERA_PERSPECTIVE;
 
-	Vector3 cubePosition = { 0.0f, 0.0f, 0.0f };
+	int cameraMode = CAMERA_FIRST_PERSON;
 
 	SetTargetFPS(60);
 
@@ -26,10 +29,9 @@ int main(void)
 
 			BeginMode3D(camera);
 				
-				DrawSphere(cubePosition, 2.0f, BLUE);
-				DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
-
-				DrawGrid(10, 1.0f);
+				// figure out a way to decouple frame time wiht movement
+				updateCameraTarget(&camera.target);
+				DrawPlane(origin, (Vector2){16.0f, 16.0f}, BROWN);
 
 			EndMode3D();
 
@@ -39,6 +41,10 @@ int main(void)
 
 		EndDrawing();
 	}
-
 	return 0;
+}
+void updateCameraTarget(Vector3 *cameratarget)
+{
+	if (IsKeyDown(KEY_LEFT)) cameratarget->x -= 0.5f;
+	if (IsKeyDown(KEY_RIGHT)) cameratarget->x += 0.5f;
 }
