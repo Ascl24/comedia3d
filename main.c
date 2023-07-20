5d1ab6d722ca1cf05ce968db9e1ca8a6275d0084
nobleboy <63758789+Ascl24@users.noreply.github.com>
2023-07-20 11:03:35 -0500

    Successfully changed camera.position so that we are always centered on the tile

--- main.c
+++ main.c
@@ -46,7 +46,7 @@ int main(void)
 	
 	// Camera Parameters
 	Camera3D camera = { 0 };
-	camera.position = (Vector3){0.0f, 2.0f, 1.0f};
+	camera.position = (Vector3){0.0f, 2.0f, 10.0f};
 	camera.target = (Vector3){0.0f, 2.0f, 0.0f };
 	camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
 	camera.fovy = 60.0f;
